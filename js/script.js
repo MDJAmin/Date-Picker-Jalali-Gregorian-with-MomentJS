5332940ce54e11588b01e5d670808d7e0525e420
Α₥Ψη <mohammadamindf@gmail.com>
2024-08-05 12:48:56 +0330

    fix the btn

--- js/script.js
+++ js/script.js
@@ -13,8 +13,8 @@ function renderCalendar() {
   const totalDays = endOfMonth.date();
 
   document.getElementById("current-month").textContent = useJalaali
-    ? currentDate.format("jMMMM jYYYY")
-    : currentDate.format("MMMM YYYY");
+    ? currentDate.format("jMMMM jYYYY")  
+    : currentDate.format("MMMM YYYY");  
 
   for (let i = 0; i < startDay; i++) {
     const emptyCell = document.createElement("div");
@@ -29,8 +29,8 @@ function renderCalendar() {
     dayCell.addEventListener("click", () => {
       const selectedDate = currentDate.clone().date(i);
       document.getElementById("selected-date").value = useJalaali
-        ? selectedDate.format("jYYYY/jMM/jDD")
-        : selectedDate.format("YYYY/MM/DD");
+        ? selectedDate.format("jYYYY/jMM/jDD") 
+        : selectedDate.format("YYYY/MM/DD");  
       document.getElementById("calendar").style.display = "none";
     });
     daysContainer.appendChild(dayCell);
@@ -82,7 +82,7 @@ document.getElementById("toggle-date").addEventListener("click", () => {
 function switchCalendarType() {
   currentDate = useJalaali
     ? moment(currentDate.format("YYYY-MM-DD"), "YYYY-MM-DD").locale("fa")
-    : moment(currentDate.format("jYYYY-jMM-jDD"), "jYYYY-jMM-jDD").locale("en");
+    : moment(currentDate.format("jYYYY/jMM/jDD"), "jYYYY/jMM/jDD").locale("en");
   renderCalendar();
 }
 
